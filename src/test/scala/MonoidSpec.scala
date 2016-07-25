06700ff95a70c82113e0083a23e55131e2b94e8b
ronnie-o <sungmin.ooh@gmail.com>
2016-07-25 18:11:45 +0900

    monoid

---
+++ src/test/scala/MonoidSpec.scala
@@ -0,0 +1,40 @@
+import org.scalatest.{Matchers, WordSpec}
+
+/**
+  * Created by ronnie on 2016. 7. 20..
+  */
+class MonoidSpec extends WordSpec with Matchers {
+
+  "Monoid" should {
+    "ex" in {
+
+      import cats._
+      import cats.std.all._
+      import cats.syntax.all._
+      
+      Monoid[String].empty shouldBe ""
+      Monoid[String].combineAll(List("a", "b", "c")) shouldBe "abc"
+      Monoid[String].combineAll(List()) shouldBe ""
+
+      Monoid[Map[String, Int]].combineAll(List(Map("a" -> 1, "b" -> 2), Map("a" -> 3))) shouldBe Map("a" -> 4, "b" -> 2)
+      Monoid[Map[String, Int]].combineAll(List()) shouldBe Map()
+
+      val l = List(1, 2, 3, 4, 5)
+      l.foldMap(identity) shouldBe 15
+      l.foldMap(i => i.toString) shouldBe "12345"
+
+      implicit def monoidTuple[A: Monoid, B: Monoid]: Monoid[(A, B)] =
+        new Monoid[(A, B)] {
+          def combine(x: (A, B), y: (A, B)): (A, B) = {
+            val (xa, xb) = x
+            val (ya, yb) = y
+            (Monoid[A].combine(xa, ya), Monoid[B].combine(xb, yb))
+          }
+          def empty: (A, B) = (Monoid[A].empty, Monoid[B].empty)
+        }
+
+      val ll = List(1, 2, 3, 4, 5)
+      ll.foldMap(i ⇒ (i, i.toString)) shouldBe (15, "12345")
+    }
+  }
+}
